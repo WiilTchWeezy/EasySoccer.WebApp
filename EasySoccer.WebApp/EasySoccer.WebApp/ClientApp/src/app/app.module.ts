be6e31ff5f79820282859c4415077861bccc76e1
Tarcísio Vitor de Souza <tarcisiovity@hotmail.com>
2019-02-22 07:23:16 -0300

    Layout Changes

--- EasySoccer.WebApp/EasySoccer.WebApp/ClientApp/src/app/app.module.ts
+++ EasySoccer.WebApp/EasySoccer.WebApp/ClientApp/src/app/app.module.ts
@@ -5,31 +5,22 @@ import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { NavMenuComponent } from './nav-menu/nav-menu.component';
-import { HomeComponent } from './home/home.component';
-import { CounterComponent } from './counter/counter.component';
-import { FetchDataComponent } from './fetch-data/fetch-data.component';
-
+import { HomeComponent } from './components/home/home.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { AuthService } from './service/auth.service';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    NavMenuComponent,
-    HomeComponent,
-    CounterComponent,
-    FetchDataComponent
-  ],
-  imports: [
-    BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
-    HttpClientModule,
-    FormsModule,
-    RouterModule.forRoot([
-      { path: '', component: HomeComponent, pathMatch: 'full' },
-      { path: 'counter', component: CounterComponent },
-      { path: 'fetch-data', component: FetchDataComponent },
-    ])
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [ AppComponent, HomeComponent, DashboardComponent ],
+	imports: [
+		BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
+		HttpClientModule,
+		FormsModule,
+		RouterModule.forRoot([
+			{ path: '', component: HomeComponent, pathMatch: 'full' },
+			{ path: 'dashboard', component: DashboardComponent }
+		])
+	],
+	providers: [ AuthService ],
+	bootstrap: [ AppComponent ]
 })
-export class AppModule { }
+export class AppModule {}
